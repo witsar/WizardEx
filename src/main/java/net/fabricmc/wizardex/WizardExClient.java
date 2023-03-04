b3bee5c7fa533ee8e3c412c063e8eb4ca3882cfc
CleverNucleus <jamie@mclean007.plus.com>
2023-03-04 12:27:17 +0000

    Minimal changes for functionality

---
+++ src/main/java/net/fabricmc/wizardex/WizardExClient.java
@@ -0,0 +1,23 @@
+package net.fabricmc.wizardex;
+
+import com.github.clevernucleus.playerex.api.client.PageRegistry;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class WizardExClient implements ClientModInitializer {
+	private static final Identifier magicIcon = new Identifier("wizardex:textures/gui/staff.png");
+	private static final Identifier magicPage = new Identifier("wizardex", "magic");
+	
+	@Override
+	public void onInitializeClient() {
+		// TODO Auto-generated method stub
+		registerSpellPowerPage();
+	}
+	
+	private void registerSpellPowerPage() {
+		PageRegistry.registerPage(magicPage, magicIcon, Text.of("Magic"));
+		PageRegistry.registerLayer(magicPage, MagicPageLayer::new);
+	}
+}
